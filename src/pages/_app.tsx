6639863bcb40e68b3f1505fcfd7abc68607feeb1
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-05-18 18:02:29 +0100

    added notification toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Layout from "~/components/layout";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -17,6 +18,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <Layout>
           <Component {...pageProps} />
         </Layout>
+        <Toaster position="bottom-right" />
       </SessionProvider>
     </main>
   );
